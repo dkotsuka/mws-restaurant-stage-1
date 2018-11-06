dd0ffc573f07f9bb96db0dde88163ecf9c6cdd87
dkotsuka <dkotsuka@gmail.com>
2018-11-05 23:15:05 -0200

    test: testando o service worker
    Correção do link

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`https://dkotsuka.github.io/mws-restaurant-stage-1/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
