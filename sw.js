303f07d032743224ed2254b35b6d48c48966be12
dkotsuka <dkotsuka@gmail.com>
2018-11-05 22:23:44 -0200

    test: testando serviceworker
    Corrigindo alguns bugs do teste do service worker.

--- sw.js
+++ sw.js
@@ -2,11 +2,14 @@ this.addEventListener("install", function (event) {
 	event.waitUntil(
 	    caches.open("v1-05-11-2018").then(function(cache) {
 	      return cache.addAll([
-	      	"/mws-restaurant-stage-1/",
+	    	"/mws-restaurant-stage-1/css/styles.css",
 	      	"/mws-restaurant-stage-1/data/restaurants.json",
-	      	"/mws-restaurant-stage-1/img/",
+	      	"/mws-restaurant-stage-1/img/1.jpg",
 	      	"/mws-restaurant-stage-1/js/main.js",
-	      	"/mws-restaurant-stage-1/js/restaurant_info.js"
+	      	"/mws-restaurant-stage-1/js/dbhelper.js",
+	      	"/mws-restaurant-stage-1/js/restaurant_info.js",
+	      	"/mws-restaurant-stage-1/index.html",
+	      	"/mws-restaurant-stage-1/restaurant.html"
 	      ]);
 	    })
 	);
